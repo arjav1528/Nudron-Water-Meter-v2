adc528173b412d308a18e74e7c7033a26222fbd7
Arjav Patel <arjav1528@gmail.com>
2025-12-20 10:00:39 +0530

    migrated date_picker_plus to a local package

---
+++ local_packages/date_picker_plus/lib/src/shared/device_orientation_builder.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart' hide DeviceOrientationBuilder;
+
+/// Builds a widget tree that can depend on the device orientation.
+class DatePickerOrientationBuilder extends StatelessWidget {
+  /// Builds a widget tree that can depend on the device orientation.
+  final Widget Function(BuildContext context, Orientation orientation) builder;
+
+  /// Builds a widget tree that can depend on the device orientation.
+  const DatePickerOrientationBuilder({
+    super.key,
+    required this.builder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.orientationOf(context);
+    return builder(context, orientation);
+  }
+}
